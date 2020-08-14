f718a582fbb01f5a05c74cacf681e662b19b2df9
Yoodahun <dahun4032@gmail.com>
2020-08-15 00:12:08 +0900

    add global properties

--- src/test/java/com/blog/tty4032/base/Base.java
+++ src/test/java/com/blog/tty4032/base/Base.java
@@ -6,22 +6,36 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class Base {
 
     public static AndroidDriver<AndroidElement> initializeCapabilities(String app) {
         // write your code here
+        Properties properties = null;
+            FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("src/test/resources/global.properties");
+            properties = new Properties();
+            properties.load(fis);
 
-        File file = new File("/Users/yoodahun/Documents/Github/Java/Appium -Mobile Automation Testing from Scratch/Library/"
-                            + app);
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+
+        File file = new File(
+                "/Users/yoodahun/Documents/Github/Java/Appium -Mobile Automation Testing from Scratch/Library/"
+                            + properties.getProperty(app));
 
         AndroidDriver<AndroidElement> driver = null;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"PracticeEmul");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,properties.getProperty("device"));
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 10);
         capabilities.setCapability(MobileCapabilityType.APP,
