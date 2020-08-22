ab1059f248a2b6d57a2886254d75431bafd9dba4
Yoodahun <dahun4032@gmail.com>
2020-08-22 22:51:17 +0900

    setting maven

--- src/test/java/com/blog/tty4032/TestEcommerce.java
+++ src/test/java/com/blog/tty4032/TestEcommerce.java
@@ -124,7 +124,7 @@ public class TestEcommerce extends Base {
         );
 
 
-        Assert.assertTrue(false);
+//        Assert.assertTrue(false);
         System.out.println("5 --------------------------");
 
         appiumDriverLocalService.stop();
